9fe8b93c20be53a9a6c83eb9703b5c71485fe826
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-10-07 03:03:55 -0700

    usuarios y productos implementación basica

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use('/api/pedidos', require('./routes/pedidosRoutes'))
+app.use('/api/usuarios', require('./routes/usuariosRoutes'))
+app.use('/api/productos', require('./routes/productosRoutes'))
 
 app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`))
