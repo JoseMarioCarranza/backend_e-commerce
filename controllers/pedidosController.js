22717f5583ed8fd592d512736858c5fc12abb0cc
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-10-07 03:05:40 -0700

    Arreglamos errores de tipo

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -5,10 +5,10 @@ const setPedidos = (req, res) => {
     res.status(201).json({ message: 'Crear pedido' })
 }
 const updatePedidos = (req, res) => {
-    res.status(201).json({ message: `Modificar tarea ${req.params.id}` })
+    res.status(201).json({ message: `Modificar pedido ${req.params.id}` })
 }
 const deletePedidos = (req, res) => {
-    res.status(201).json({ message: `Eliminar tarea ${req.params.id}` })
+    res.status(201).json({ message: `Eliminar pedido ${req.params.id}` })
 }
 
 module.exports = {
