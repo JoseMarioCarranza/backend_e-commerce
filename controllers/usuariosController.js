d02cbcc9d2562365a50eb13b355a6225a72e71ea
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-10-08 13:47:46 -0700

    type errors

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -8,8 +8,13 @@ const getData = (req, res) => {
     res.status(200).json({ message: `Datos del usuario` })
 }
 
+const makeAdmin = (req, res) => {
+    res.status(200).json({ message: `Usuario con id ${req.params.id} convertido en administrador` })
+}
+
 module.exports = {
     login,
     singUp,
-    getData
+    getData,
+    makeAdmin
 }
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { login, singUp, getData } = require('../controllers/usuariosController')
+const { login, singUp, getData, makeAdmin } = require('../controllers/usuariosController')
 
 router.post('/login', login)
 
 router.post('/singup', singUp)
 
 router.get('/data', getData)
+
+router.put('/makeadmin/:id', makeAdmin)
 
 module.exports = router
