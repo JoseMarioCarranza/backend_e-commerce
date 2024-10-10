1de7fe6a0335356ba7006d2451bcbc32b49f36e5
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-10-10 02:46:24 -0700

    Logging con JWT

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -3,10 +3,6 @@ const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../model/userModel')
 
-const login = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Inicio de sesión correcto' })
-})
-
 const singUp = asyncHandler(async (req, res) => {
 
     // Desglosamos los datos del body
@@ -52,6 +48,34 @@ const singUp = asyncHandler(async (req, res) => {
 
 })
 
+const login = asyncHandler(async (req, res) => {
+
+    //desestructuramos el body
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        res.status(400)
+        throw new Error('Falta de introducir datos')
+    }
+
+    //verificamos que exista el usuario
+    const user = await User.findOne({ email })
+
+    //verificamos al usuario y a la contraseña
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generarToken(user.id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Credenciales incorrectas')
+    }
+
+})
+
 const getData = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Datos del usuario` })
 })
@@ -59,6 +83,12 @@ const getData = asyncHandler(async (req, res) => {
 const makeAdmin = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Usuario con id ${req.params.id} convertido en administrador` })
 })
+
+const generarToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
 
 module.exports = {
     login,
