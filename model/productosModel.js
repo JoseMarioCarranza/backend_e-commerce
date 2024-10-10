ff3b0806048de0b4e2a3b9454a7ef2be49956a24
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-10-09 17:27:37 -0700

    linkeamos tabla de usuarios con productos

--- model/productosModel.js
+++ model/productosModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const productoSchema = mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     nombre: { type: String, required: [true, 'Por favor teclea un nombre del producto'] },
     precio: { type: Number, required: [true, 'Por favor teclea el precio del producto'] },
 },
